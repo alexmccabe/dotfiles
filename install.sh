ed13da508c51fed3ad1e573f7509ba5c2d26fabb
Alex McCabe <alexwmccabe@gmail.com>
2018-08-24 15:22:08 +0100

    Add: Install script

---
+++ install.sh
@@ -0,0 +1,75 @@
+# !/bin/bash
+
+if [ -d ~/.scm_breeze ]; then
+    echo 'SCM_BREEZE is already installed, skipping'
+else
+    git clone git://github.com/scmbreeze/scm_breeze.git ~/.scm_breeze
+fi
+# adapted from https://github.com/skeeto/dotfiles/blob/master/install.sh
+
+## Install each _-prefixed file or dir
+# find . -type f -name "_*" -print0 | sort -z | while read -d $'\0' file
+# do
+#     dotfile=${file/.\/_/.}
+
+#     ## Install directory first
+#     if [ ! -e $(dirname ~/$dotfile) ]; then
+#     	echo "create directory: "
+#     	echo $(dirname ~/$dotfile)
+#         mkdir -p -m 700  $(dirname ~/$dotfile)
+#     fi
+
+#     ## Create a link to the repository version
+#     echo Installing $dotfile
+#     ln -fs $(pwd)/$file ~/$dotfile
+#     chmod go-rwx $file
+# done
+
+split() {
+    path="$1"
+    output=""
+
+    while IFS='/' read -ra SPLIT; do
+        for i in "${SPLIT[@]}"; do
+            if [ "$i" = "." ]; then
+                output="$output"
+            else
+                if [[ "$i" == _* ]]; then
+                    output="$output/.${i#_*}"
+                else
+                    output="$output/$i"
+                fi
+            fi
+        done
+    done <<< "$path"
+
+    echo "$output"
+}
+
+install() {
+    dotfile="$1"
+    dest=$(split "$dotfile")
+
+    dest="$HOME/.${dest#./.*}"
+
+    if [ "$dotfile" = "${dotfile##*.enchive}" ]; then
+        echo Installing "$dotfile"
+
+        mkdir -p -m 700 "$(dirname "$dest")"
+
+        chmod go-rwx "$dotfile"
+
+        ln -fs $lnflags "$(pwd)/$dotfile" "$dest"
+    fi
+}
+
+## Install each _-prefixed file
+for source in $(find . -name '_*' | sort); do
+    if [ -d "$source" ]; then
+        for dotfile in $(find "$source" -type f | sort); do
+            install "$dotfile"
+        done
+    else
+        install "$source"
+    fi
+done
